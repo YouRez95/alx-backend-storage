b0d6a73aaaea22b000a569b59b11fe06302fbc33
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-25 20:39:19 +0100

    compute average score using procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users
-    SET average_score = (SELECT SUM(score) FROM corrections WHERE users.id = user_id) / (SELECT COUNT(score) FROM corrections WHERE users.id = user_id)
+    SET average_score = (SELECT SUM(score) FROM corrections WHERE corrections.user_id = user_id) / (SELECT COUNT(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE users.id = user_id;
 END $$
 DELIMITER ;
