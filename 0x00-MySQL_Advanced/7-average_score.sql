fdc4bd22381ffee7b186981c7b609546580c299e
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-25 20:33:42 +0100

    compute average score using procedure

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	UPDATE users
+    SET average_score = (SELECT SUM(score) FROM corrections WHERE users.id = user_id) / (SELECT COUNT(score) FROM corrections WHERE users.id = user_id)
+    WHERE users.id = user_id;
+END $$
+DELIMITER ;
