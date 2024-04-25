e251aa6283a5dd9bcc6bd248577764c4e3a57f90
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-25 15:02:17 +0100

    select column from table using SUM

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT DISTINCT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
