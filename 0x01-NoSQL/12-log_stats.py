fb327af9ce4eb3cefaf77ad6188ee2af17040e08
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-30 14:51:51 +0100

    use the methods list

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,17 +9,11 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
     num_logs = logs.count_documents({})
-    num_get = logs.count_documents({"method": "GET"})
-    num_post = logs.count_documents({"method": "POST"})
-    num_put = logs.count_documents({"method": "PUT"})
-    num_patch = logs.count_documents({"method": "PATCH"})
-    num_delete = logs.count_documents({"method": "DELETE"})
-    num_get_status = logs.count_documents({"method": "GET", "path": "/status"})
     print("{} logs".format(num_logs))
     print("Methods:")
-    print("\tmethod GET: {}".format(num_get))
-    print("\tmethod POST: {}".format(num_post))
-    print("\tmethod PUT: {}".format(num_put))
-    print("\tmethod PATCH: {}".format(num_patch))
-    print("\tmethod DELETE: {}".format(num_delete))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, num_method))
+    num_get_status = logs.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(num_get_status))
