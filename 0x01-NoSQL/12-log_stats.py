f3579c65e624ad2c5ec776388a9690f1af9de543
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-30 15:05:58 +0100

    use the methods list

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 """
-log stats
+print log stats
 """
 
 from pymongo import MongoClient
 
 if __name__ == "__main__":
+    """
+      Python script that provides
+      some stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
     num_logs = logs.count_documents({})
-    print("{} logs".format(num_logs))
-    print("Methods:")
+    print('{} logs'.format(num_logs))
+    print('Methods:')
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         num_method = logs.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, num_method))
+        print('\tmethod {}: {}'.format(method, num_method))
     num_get_status = logs.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(num_get_status))
+    print('{} status check'.format(num_get_status))
