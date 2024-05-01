7d6879640a30be63526ad49a67a545ef566be497
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-01 15:55:09 +0100

    reading from redis and converting to the original format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -21,3 +21,20 @@ class Cache:
         id = str(uuid.uuid1())
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if value and fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """convert the data back to the string format"""
+        return self._redis.get(key)
+
+    def get_int(self, key: str) -> int:
+        """convert the data back to the integer format"""
+        value = self._redis.get(key)
+        return int(value)
